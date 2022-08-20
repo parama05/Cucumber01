e46670df868156f71f0ce65d6451c90b6d57c015
parama05 <paramatanjore@gmail.com>
2022-08-20 19:19:30 +0530

    added demo method

---
+++ src/test/java/com/demo/Company.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class Company {
+	
+	public void m1() {
+		System.out.println("test1");
+	}
+	
+	public void m2() {
+		System.out.println("test2");
+	}
+	
+	public void m3() {
+		System.out.println("test3");
+	}
+
+}
